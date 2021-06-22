b0f6560e92c33ff252f566ad47bd9ba9dded1d5c
OmegaRogue <omegarogue@omegavoid.codes>
2021-06-22 16:27:09 +0200

    :sparkles: Add Serial output of button state

--- Blinker/src/main.cpp
+++ Blinker/src/main.cpp
@@ -1,28 +1,32 @@
 #include <Arduino.h>
-
-const byte LED = 13;
-const byte Taster = 4;
+const byte LED=13;
+const byte Taster=4;
 int TasterWert;
+boolean x=true;
 
 
 void setup() {
-//Serial.begin(9600);
-    pinMode(LED, OUTPUT); // Pin2 als Ausgang definieren
-    pinMode(Taster, INPUT_PULLUP); // Pin4 als Eingang definieren
+    Serial.begin(9600);
+    pinMode(LED,OUTPUT);//Pin2 als Ausgang definieren
+    pinMode(Taster,INPUT_PULLUP);//Pin4 als Eingang definieren
 //digitalWrite (Taster,HIGH);
-//Serial.println(LED_BUILTIN);
+
 }
 
 
 void loop() {
-    TasterWert = digitalRead(Taster);// Taster lesen
-    if (TasterWert == true) {
-        digitalWrite(LED, HIGH);// Port2 auf High setzen
-        delay(500);// 500ms warten
-        digitalWrite(LED, LOW);// Port2 auf Low setzen
-        delay(500);// 500ms warten
-    } else {
-        digitalWrite(LED, LOW);// Port2 auf Low setzen
+    TasterWert=digitalRead(Taster);//Taster lesen
+    if (TasterWert==true)
+    {
+        digitalWrite(LED,x);//Port2 auf High setzen
+        delay(500);//500ms warten
+        x = !x;
+        Serial.print(x);
+//    digitalWrite(LED,LOW);//Port2 auf Low setzen
+//    delay(500);//500ms warten
+    }
+    else{
+        digitalWrite(LED,LOW);//Port2 auf Low setzen
     }
 
 }
