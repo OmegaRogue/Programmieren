034bcefbc34ade88599e351d00f4b46f11535fb9
OmegaRogue <omegarogue@omegavoid.codes>
2021-06-22 15:48:41 +0200

    :art: Add Spaces in comments

--- Blinker/src/main.cpp
+++ Blinker/src/main.cpp
@@ -7,22 +7,22 @@ int TasterWert;
 
 void setup() {
 //Serial.begin(9600);
-    pinMode(LED, OUTPUT);//Pin2 als Ausgang definieren
-    pinMode(Taster, INPUT_PULLUP);//Pin4 als Eingang definieren
+    pinMode(LED, OUTPUT); // Pin2 als Ausgang definieren
+    pinMode(Taster, INPUT_PULLUP); // Pin4 als Eingang definieren
 //digitalWrite (Taster,HIGH);
 //Serial.println(LED_BUILTIN);
 }
 
 
 void loop() {
-    TasterWert = digitalRead(Taster);//Taster lesen
+    TasterWert = digitalRead(Taster);// Taster lesen
     if (TasterWert == true) {
-        digitalWrite(LED, HIGH);//Port2 auf High setzen
-        delay(500);//500ms warten
-        digitalWrite(LED, LOW);//Port2 auf Low setzen
-        delay(500);//500ms warten
+        digitalWrite(LED, HIGH);// Port2 auf High setzen
+        delay(500);// 500ms warten
+        digitalWrite(LED, LOW);// Port2 auf Low setzen
+        delay(500);// 500ms warten
     } else {
-        digitalWrite(LED, LOW);//Port2 auf Low setzen
+        digitalWrite(LED, LOW);// Port2 auf Low setzen
     }
 
 }
